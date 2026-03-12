f7a7b41e4610d0e7a658f7d3c565468cafd1c80f
LAPTOP-64H96TPQ\10493 <1193677504@qq.com>
2026-03-12 10:19:44 +0800

    基本架构

---
+++ blade-fire/index.js
@@ -0,0 +1,28 @@
+class BladeFire {
+    version = "1.0.0";
+    static init() {
+        console.log("BladeFire initialized");
+    }
+
+    static line() {
+        console.log("Draw a line");
+    }
+    static circle() {
+        console.log("Draw a circle");
+    }
+    static rect() {
+        console.log("Draw a square");
+    }
+    static triangle() {
+        console.log("Draw a triangle");
+    }
+    static polygon() {
+        console.log("Draw a polygon");
+    }
+    static text() {
+        console.log("Draw a text");
+    }
+    static image() {
+        console.log("Draw an image");
+    }
+}
